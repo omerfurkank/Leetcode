63c2b94bb4c46508723b37f2dcae4a48a9c1d2ac
omerfurkank <omerfurkankilic@yandex.com>
2023-03-05 21:30:20 +0300

    two sum o(n) time

--- Leetcode/TwoSum.cs
+++ Leetcode/TwoSum.cs
@@ -26,5 +26,21 @@ namespace Leetcode
             }
             return result;
         }
+        public int[] TwoSumSolution2(int[] nums, int target)
+        {
+         var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(target - nums[i]))
+                {
+                    return new int[] {dict[target - nums[i]],i };
+                }
+                if (!dict.ContainsKey(nums[i]))
+                {
+                    dict.Add(nums[i], i);
+                }
+            }
+         return new int[3];
+        }
     }
 }
