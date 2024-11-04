5d33662458306b6438c124065087b0b23ae8606c
omerfurkank <omerfurkandev@gmail.com>
2024-11-04 13:15:46 +0300

    TwoSum

---
+++ Leetcode/Arrays/TwoSum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Arrays;
+
+public static class TwoSum
+{
+    //brute force
+    public static int[] TwoSum1(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == target) return new int[] { i, j };
+            }
+        }
+        return new int[2];
+    }
+
+    //hashset
+    public static int[] TwoSum2(int[] nums, int target)
+    {
+        var set = new HashSet<int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if(set.Contains(target-nums[i])) return new int[] {i, Array.IndexOf(nums, target- nums[i]) };
+            else set.Add(nums[i]);
+        }
+        return new int[2];
+    }
+
+    //dictionary
+    public static int[] TwoSum3(int[] nums, int target)
+    {
+        var dict = new Dictionary<int,int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(target - nums[i])) return new int[] { i, dict[target - nums[i]] };
+            if (!dict.ContainsKey(nums[i])) dict.Add(nums[i],i);
+        }
+        return new int[2];
+    }
+}
--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Leetcode.Arrays;
+
+Console.WriteLine("Hello, World!");
+
+int[] numbers = TwoSum.TwoSum3([3, 2, 4], 6);
+Console.WriteLine(string.Join(", ",numbers));
+
