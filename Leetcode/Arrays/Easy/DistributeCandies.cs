b89a03d35518183f0635927caee8bca45a58b899
omerfurkank <omerfurkandev@gmail.com>
2024-12-02 17:52:06 +0300

    DistributeCandies

---
+++ Leetcode/Arrays/Easy/DistributeCandies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Arrays.Easy;
+public static class DistributeCandies
+{
+    public static int DistributeCandies1(int[] candyType)
+    {
+        int uniqueCandies = candyType.ToHashSet().Count;
+        int canEat = candyType.Length/2;
+
+        return uniqueCandies > canEat ? canEat : uniqueCandies;
+    }
+}
