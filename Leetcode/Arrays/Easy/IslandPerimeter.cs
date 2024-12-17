118d2c6102bfe1a94a52ec537aa42d11809230ff
omerfurkank <omerfurkandev@gmail.com>
2024-12-17 09:44:06 +0300

    IslandPerimeter

---
+++ Leetcode/Arrays/Easy/IslandPerimeter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Arrays.Easy;
+public static class IslandPerimeter
+{
+    public static int IslandPerimeter1(int[][] grid)
+    {
+        int perimeter = 0;
+        int rows = grid.Length;
+        int cols = grid[0].Length;
+
+        for (int i = 0; i < grid.Length; i++)
+        {
+            for (int j = 0; j < grid[i].Length; j++)
+            {
+                if (grid[i][j] == 1)
+                {
+                    perimeter += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) perimeter -= 2;
+                    if (j > 0 && grid[i][j - 1] == 1) perimeter -= 2;
+                }
+            }
+        }
+        return perimeter;
+    }
+}
