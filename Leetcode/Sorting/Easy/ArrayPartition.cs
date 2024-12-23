7bf1264ac967d7a8fe383e45d6303eae378ecd25
omerfurkank <omerfurkandev@gmail.com>
2024-12-23 14:30:19 +0300

    ArrayPartition

---
+++ Leetcode/Sorting/Easy/ArrayPartition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Sorting.Easy;
+public static class ArrayPartition
+{
+    public static int ArrayPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.Length; i += 2) result += Math.Min(nums[i], nums[i + 1]);
+        return result;
+    }
+}
+
