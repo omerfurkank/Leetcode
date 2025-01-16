e293e906a7edb408dc0a7d91dbfc9877cfa8845d
furkan.kilic <furkan.kilic@tusdata.com>
2025-01-16 09:53:56 +0300

    PlusOne

---
+++ Leetcode/Arrays/Easy/PlusOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Leetcode.Arrays.Easy;
+public static class PlusOne
+{
+    public static int[] PlusOne1(int[] digits)
+    {
+        for (int i = digits.Length - 1; i > -1; i--)
+        {
+            if (digits[i] != 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.Length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
--- Leetcode/Arrays/Easy/SingleNumber.cs
+++ Leetcode/Arrays/Easy/SingleNumber.cs
@@ -14,13 +14,10 @@ public static class SingleNumber
         if (nums.Length == 1) return nums[0];
 
         if (nums[0] != nums[1]) return nums[0];
+
         for (int i = 1; i < nums.Length - 1; i++)
-        {
-            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1])
-            {
-                return nums[i];
-            }
-        }
+            if (nums[i] != nums[i - 1] && nums[i] != nums[i + 1]) return nums[i];
+
         return nums[nums.Length - 1];
     }
     public static int SingleNumber2(int[] nums)
@@ -32,6 +29,18 @@ public static class SingleNumber
             num = nums[i] ^ num;
 
         return num;
+    }
+    public static int SingleNumber3(int[] nums)
+    {
+        Array.Sort(nums);  // Diziyi sırala
+
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            if (nums[i] != nums[i + 1]) return nums[i];
+            else i++;  // Çift elemanları atla
+
+        }
+        return nums[nums.Length - 1];  // Son eleman tekse, onu döndür
     }
 }
 
