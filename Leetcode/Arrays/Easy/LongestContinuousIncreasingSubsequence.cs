08bc1c93f5892289e09a6c4e0aa899d2e27ae2b9
furkan.kilic <furkan.kilic@tusdata.com>
2025-02-11 09:19:16 +0300

    LongestContinuousIncreasingSubsequence

---
+++ Leetcode/Arrays/Easy/LongestContinuousIncreasingSubsequence.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Arrays.Easy;
+public static class LongestContinuousIncreasingSubsequence
+{
+    public static int FindLengthOfLCIS(int[] nums)
+    {
+        int counter = 1;
+        int max = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                counter++;
+                if (counter > max) max = counter;
+            }
+            else counter = 1;
+        }
+        return max; ;
+    }
+}
