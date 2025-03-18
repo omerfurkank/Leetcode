06b8477eeec21d56ed2c0e6c515ae5540eba2656
furkan.kilic <furkan.kilic@tusdata.com>
2025-03-18 10:21:19 +0300

    ReshapeTheMatrix

---
+++ Leetcode/Arrays/Easy/ReshapeTheMatrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.Arrays.Easy;
+
+public static class ReshapeTheMatrix
+{
+    public static int[][] MatrixReshape(int[][] mat, int r, int c)
+    {
+        bool possible = mat.Length * mat[0].Length  == r*c;
+        if (!possible) return mat;
+
+        int[][] result = new int[r][];
+        List<int> numbers = new();
+
+        for (int i = 0; i < mat.Length; i++)
+            for (int j = 0; j < mat[i].Length; j++) numbers.Add(mat[i][j]);
+
+        int k = 0;
+        for (int i = 0;i<r; i++)
+        {
+            result[i] = new int[c];
+            for(int j = 0;j < c; j++)
+            {
+                result[i][j] = numbers[k];
+                k++;
+            }
+        }
+        return result;
+}
+}
