2157620994cd5f29968928c652dd023d293b91e2
furkan.kilic <furkan.kilic@tusdata.com>
2025-05-15 15:45:03 +0300

    converted to neetcode 250

---
+++ Leetcode/1Array&Hashing/ConcatenationOfArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode._1Array_Hashing;
+
+static class ConcatenationOfArray
+{
+    public static int[] GetConcatenation(int[] nums)
+    {
+        int[] result = new int[nums.Length * 2];
+
+        for (int i = 0; i <= nums.Length - 1; i++)
+        {
+            result[i] = nums[i];
+            result[i + nums.Length] = nums[i];
+        }
+        return result;
+    }
+}
