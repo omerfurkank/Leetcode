6defe6b09965c125ac8c34dd4c64736f5544d8c5
furkan.kilic <furkan.kilic@tusdata.com>
2025-05-20 09:31:55 +0300

    ContainsDuplicate

---
+++ Leetcode/1Array&Hashing/ContainsDuplicate.cs
@@ -0,0 +1,11 @@
+﻿namespace Leetcode._1Array_Hashing;
+
+static class ContainsDuplicate
+{
+    public static bool ContainsDuplicate1(int[] nums)
+    {
+        HashSet<int> set = new();
+        foreach (var num in nums) if (!set.Add(num)) return true;
+        return false;
+    }
+}
