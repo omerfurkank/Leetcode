3e9fecfd55bab50401225d0b4b4e4647db81485a
furkan.kilic <furkan.kilic@tusdata.com>
2025-05-21 09:36:36 +0300

    TwoSum

---
+++ Leetcode/1Array&Hashing/TwoSum.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode._1Array_Hashing;
+
+public class TwoSum
+{
+    public int[] TwoSum1(int[] nums, int target)
+    {
+        Dictionary<int, int> dict = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.ContainsKey(target - nums[i])) return [dict[i], dict[target - nums[i]]];
+            else if (!dict.ContainsKey(nums[i])) dict.Add(nums[i], i);
+
+        }
+        return [0, 0];
+    }
+}
